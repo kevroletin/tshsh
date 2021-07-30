2f6ce04d8f7d3c300d095b77624e0677d51602fe
Vasiliy Kevroletin <kevroletin@gmail.com>
2021-07-30 12:55:30 +0800

    Add more benchmarks for matchers

--- bench/Main.hs
+++ bench/Main.hs
@@ -1,13 +1,12 @@
 import Data.ByteString as BS
 import Data.Strict.Tuple
-import qualified Data.Text as T
 import Gauge.Main
+import qualified Matcher.ByteString as SomeM
 import Matcher.Result
-import Matcher.Seq.ByteString
+import qualified Matcher.Seq.ByteString as SeqM
 import Protolude
 import qualified Spec.Simulator as S
 import qualified Spec.SimulatorM as SM
-import System.Process
 
 -- BS.breakSubstring uses either robin-karp with a rolling hash, or an
 -- optimization for small patterns with a byte shift and comparison of
@@ -25,24 +24,41 @@ countMatchesBS_ !res p str
 countMatchesBS :: ByteString -> ByteString -> Int
 countMatchesBS = countMatchesBS_ 0
 
-countMatches_ :: Int -> Matcher -> ByteString -> Int
+countMatches_ :: Int -> SeqM.Matcher -> ByteString -> Int
 countMatches_ !res m str
   | BS.null str = res
   | otherwise =
-    case matchStr m str of
+    case SeqM.matchStr m str of
       Match m' _ _ r -> countMatches_ (res + 1) m' r
       NoMatch _ -> res
 
-countMatchesSeq :: Matcher -> ByteString -> Int
+countMatchesSeq :: SeqM.Matcher -> ByteString -> Int
 countMatchesSeq = countMatches_ 0
+
+countMatchesSomeM_ :: Int -> SomeM.SomeMatcher -> ByteString -> Int
+countMatchesSomeM_ !res m str
+  | BS.null str = res
+  | otherwise =
+    case SomeM.matchStr m str of
+      Match m' _ _ r -> countMatchesSomeM_ (res + 1) m' r
+      NoMatch _ -> res
+
+countMatchesSomeM :: SomeM.SomeMatcher -> ByteString -> Int
+countMatchesSomeM = countMatchesSomeM_ 0
 
 manySubstrings :: Int -> ByteString -> ByteString
 manySubstrings n = BS.pack . Protolude.take n . cycle . mconcat . Protolude.inits . BS.unpack
 
+zeros :: [Word8]
+zeros = 0 : zeros
+
+zeros100k :: ByteString
+zeros100k = BS.pack $ Protolude.take (100 * 1024) zeros
+
 main :: IO ()
 main = do
-  fname <- T.strip . T.pack <$> readProcess "sh" ["-c", "find -type f -name tshsh-exe | head -n 1"] ""
-  str <- withFile (T.unpack fname) ReadMode BS.hGetContents
+  let prompt = "[[prompt]]"
+  let str = BS.concat $ Protolude.replicate 100 (prompt <> zeros100k)
 
   let longsubstring = "longsubstring"
   let str2 = manySubstrings (1024 * 1024) longsubstring
@@ -50,14 +66,18 @@ main = do
   defaultMain
     [ bgroup
         "long/a few"
-        -- This test takes a big sequence (a few MB binary) with just a few
+        -- This test takes a long sequence (a few MB binary) with just a few
         -- occurrences of a pattern. Here we test how efficiently the matcher
         -- can skip irrelevant input. Such usage pattern might happen when a
         -- user of tshsh dumps a big file to the output.
         [ bench "BS" $
-            whnf (countMatchesBS "word") str,
-          bench "Matcher.Seq" $
-            whnf (countMatchesSeq (mkMatcher "word")) str,
+            whnf (countMatchesBS prompt) str,
+          bench "SeqM.Matcher.Seq" $
+            whnf (countMatchesSeq (SeqM.mkMatcher prompt)) str,
+          bench "SeqM.Matcher seq" $
+            whnf (countMatchesSomeM (SomeM.mkSeqMatcher prompt)) str,
+          bench "SeqM.Matcher bracket" $
+            whnf (countMatchesSomeM (SomeM.mkBracketMatcher "[[" "]]")) str,
           bench "dummy BS.foldl'" $
             whnf (BS.foldl' (+) 0) str
         ],
@@ -65,30 +85,32 @@ main = do
         "many"
         [ bench "BS" $
             whnf (countMatchesBS longsubstring) str2,
-          bench "Matcher.Seq" $
-            whnf (countMatchesSeq (mkMatcher longsubstring)) str2,
+          bench "SeqM.Matcher.Seq" $
+            whnf (countMatchesSeq (SeqM.mkMatcher longsubstring)) str2,
+          bench "SeqM.Matcher seq" $
+            whnf (countMatchesSomeM (SomeM.mkSeqMatcher longsubstring)) str2,
           bench "dummy BS.foldl'" $
             whnf (BS.foldl' (+) 0) str2
+        ],
+      bgroup
+        "Coroutines DSL"
+        [ bench "CPS" $
+            whnf
+              (S.simulate (() :!: S.syncEnv))
+              [ (S.Shell_1, "env\n", "a=1\nb=2\n"),
+                (S.Shell_1, "pwd\n", "/root\n"),
+                (S.Shell_2, "export a=1\n", ""),
+                (S.Shell_2, "export b=2\n", ""),
+                (S.Shell_2, "cd '/root'\n", "")
+              ],
+          bench "Monadic" $
+            whnf
+              (SM.simulate SM.syncEnv)
+              [ (SM.Shell_1, "env\n", "a=1\nb=2\n"),
+                (SM.Shell_1, "pwd\n", "/root\n"),
+                (SM.Shell_2, "export a=1\n", ""),
+                (SM.Shell_2, "export b=2\n", ""),
+                (SM.Shell_2, "cd '/root'\n", "")
+              ]
         ]
-      -- bgroup
-      --   "Coroutines DSL"
-      --   [ bench "CPS" $
-      --       whnf
-      --         (S.simulate (() :!: S.syncEnv))
-      --         [ (S.Shell_1, "env\n", "a=1\nb=2\n"),
-      --           (S.Shell_1, "pwd\n", "/root\n"),
-      --           (S.Shell_2, "export a=1\n", ""),
-      --           (S.Shell_2, "export b=2\n", ""),
-      --           (S.Shell_2, "cd '/root'\n", "")
-      --         ],
-      --     bench "Monadic" $
-      --       whnf
-      --         (SM.simulate SM.syncEnv)
-      --         [ (SM.Shell_1, "env\n", "a=1\nb=2\n"),
-      --           (SM.Shell_1, "pwd\n", "/root\n"),
-      --           (SM.Shell_2, "export a=1\n", ""),
-      --           (SM.Shell_2, "export b=2\n", ""),
-      --           (SM.Shell_2, "cd '/root'\n", "")
-      --         ]
-      --   ]
     ]
